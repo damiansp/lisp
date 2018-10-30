ca1198f92be52f6bbbcf5ea48697c2092307f528
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-10-30 12:14:17 -0400

    +9

---
+++ emacsLisp/gnu/01_ListProcessing/9_set_setq.el
@@ -0,0 +1,17 @@
+;; set
+(set 'flowers '(rose violet daisy buttercup))
+flowers
+'flowers
+
+
+;; setq
+(setq carnivores '(lion tiger leopard))
+(setq trees '(pine fir oak maple)
+      herbivores '(gazelle antelope zebra))
+trees
+
+
+;; Counting
+(setq counter 0)
+(setq counter (+ counter 1))
+counter
