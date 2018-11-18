6b3bd7c829cb998d7fb3857634fd27fd542e2373
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-11-18 03:08:04 -0500

    +defun

---
+++ emacsLisp/gnu/03_writingDefuns/02_defun.el
@@ -0,0 +1,11 @@
+; Syntax
+;(defun function-name(args...)
+;  "optional docstring..."
+;  (interactive arg-passing info) ; optional
+;  body...)
+
+(defun multiply-by-seven (n)
+  "Multipy N by seven"
+  (* 7 n))
+
+(multiply-by-seven 3)
